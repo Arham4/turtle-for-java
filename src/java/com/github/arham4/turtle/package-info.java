d8c36488195023d5b2fc7dfedf4d9303bdeee8a5
Arham4 <arhamjsiddiqui@gmail.com>
2019-07-05 15:31:09 -0500

    Add package-infos

---
+++ src/java/com/github/arham4/turtle/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * The package in which all core classes related to the Turtle API exist.
+ */
+package com.github.arham4.turtle;
---
+++ src/java/com/github/arham4/turtle/utils/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Helper classes to assist one's development in the Turtle API.
+ */
+package com.github.arham4.turtle.utils;
